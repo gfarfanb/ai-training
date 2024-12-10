f189ad535e2adbbecd6f00a675d740a68281a22b
Giovanni Farfán B. <gfarfanb@gmail.com>
2024-12-10 11:20:11 -0600

    Remove Python

--- python/automate-the-boring-stuff-with-python/helloFunc.py
+++
@@ -1,9 +0,0 @@
-
-def hello(name):
-    print('Howdy!', end='<br>')
-    print('Howdy!!!')
-    print('Hello', 'there', '.', sep='|')
-    print('Hello, ' + name)
-
-
-hello('py')
--- python/automate-the-boring-stuff-with-python/locateZoom.py
+++
@@ -1,6 +0,0 @@
-import pyautogui, time
-
-# Active Firefox window (on PDF viewer)
-time.sleep(5)
-# Click on Automatic Zoom button
-pyautogui.click('images/zoom.png')
